82dbc8e76940b7931836ec496660fc297b6c9189
Stanley_Chueh <stanleychueh28@gmail.com>
2024-06-09 00:28:31 +0800

    6/9
    Fix the problem of receiving goal pose
    
    Originally,  i include function (isNavComplete) from sample_navigator.py for navigation success info, but the info i received is so weird that i try multiple times but keep failing.
    Therefore,I try different approach:Use amclpose to see whether it reach goalpose or not.That's because when the robot stops, it'll stop publishing amclpose, so in this way, i set a 10 timer to check whether there is no new update amclpose or not. No new data for 10 second means that it reach the goal, otherwise it still moving.

--- multi-map_navigation_V2/custom_nav/custom_nav/automation_script.py
+++ multi-map_navigation_V2/custom_nav/custom_nav/automation_script.py
@@ -12,6 +12,8 @@ class AutomatedNavigation(Node):
         self.navigator = BasicNavigator()
         self.initial_pose_received = False
         self.navigation_succeeded = False
+        self.last_amcl_pose_time = None
+        self.pose_update_timeout = 10  # seconds
 
         self.create_subscription(
             PoseWithCovarianceStamped,
@@ -19,60 +21,70 @@ class AutomatedNavigation(Node):
             self.initialpose_callback,
             10)
         
+        self.create_subscription(
+            PoseWithCovarianceStamped,
+            '/amcl_pose',
+            self.amclpose_callback,
+            10)
+
         self.run_sequence()
 
     def initialpose_callback(self, msg):
         self.initial_pose_received = True
-        self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
+        self.get_logger().info('Initial pose received')
+
+    def amclpose_callback(self, msg):
+        self.last_amcl_pose_time = time.time()
+        self.get_logger().info('Received amcl_pose message')
+        self.get_logger().info('Updated last_amcl_pose_time to: {}'.format(self.last_amcl_pose_time))
 
     def run_sequence(self):
         # Run client 1 0
-        self.get_logger().info('Start !!')
-        self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
-        self.get_logger().info("Navigation pose received: {}".format(self.navigation_succeeded))
+        self.get_logger().info('Start sequence')
         os.system('ros2 run custom_nav client 1 0')
         self.wait_for_initial_pose()
         
         # Run client 2 0
-        self.get_logger().info('Start first navigation !!')
-        self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
-        self.get_logger().info("Navigation pose received: {}".format(self.navigation_succeeded))
+        self.get_logger().info('Start first navigation')
         os.system('ros2 run custom_nav client 2 0')
         self.wait_for_navigation_success()
         
         if self.navigation_succeeded:
-            self.get_logger().info('Ready to load map2!')
-            self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
-            self.get_logger().info("Navigation pose received: {}".format(self.navigation_succeeded))
+            self.get_logger().info('Ready to load map2')
+            os.system('ros2 run custom_nav client 1 1')  
+        os.system('ros2 run custom_nav client 2 1')  
 
-            # Run client 1 1
-            os.system('ros2 run custom_nav client 1 1')            
-            
-        # Run client 2 1
-        #os.system('ros2 run custom_nav client 2 1')
-        
     def wait_for_initial_pose(self):
         self.get_logger().info('Waiting for initial pose...')
         while not self.initial_pose_received:
-            rclpy.spin_once(self)
+            rclpy.spin_once(self, timeout_sec=0.1)
         self.initial_pose_received = False
-        self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
+        self.get_logger().info('Initial pose processed')
 
     def wait_for_navigation_success(self):
         self.get_logger().info('Waiting for navigation success...')
-        # Add logic to wait for navigation success
-        # For demonstration purposes, assuming it's succeeded after waiting for some time
-        while self.navigator.isNavComplete():
-            time.sleep(1)
-            self.get_logger().info('Navigation fail!')
-            self.get_logger().info("Navigation pose received: {}".format(self.navigation_succeeded))
-            self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
-            
-        self.navigation_succeeded = True
-        self.initial_pose_received = False
-        self.get_logger().info('Navigation done!')
-        self.get_logger().info("Navigation pose received: {}".format(self.navigation_succeeded))
-        self.get_logger().info("Initial pose received: {}".format(self.initial_pose_received))
+        self.last_amcl_pose_time = time.time()
+        last_log_time = self.last_amcl_pose_time
+
+        while True:
+            rclpy.spin_once(self, timeout_sec=0.1)
+            current_time = time.time()
+            self.get_logger().info("current_time: {}".format(current_time))
+            self.get_logger().info("self.last_amcl_pose_time: {}".format(self.last_amcl_pose_time))
+            self.get_logger().info("current_time - self.last_amcl_pose_time: {}".format(current_time - self.last_amcl_pose_time))
+            if current_time - self.last_amcl_pose_time > self.pose_update_timeout:
+                self.get_logger().info('No update in amcl_pose for 5 seconds, considering navigation success.')
+                self.navigation_succeeded = True
+                break
+            elif current_time - last_log_time > 1:  # Log every 1 second
+                self.get_logger().info('Still waiting for amcl_pose update...')
+                last_log_time = current_time
+            time.sleep(0.1)  # Use a smaller sleep interval for better responsiveness
+
+        if self.navigation_succeeded:
+            self.get_logger().info('Navigation succeeded')
+        else:
+            self.get_logger().info('Navigation failed')
 
 def main(args=None):
     rclpy.init(args=args)
@@ -83,4 +95,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
